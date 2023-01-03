1bb5a2b89cd8df594aa15bbc100ca95a19d8ca83
Christian Panter <pantercp@outlook.com>
2023-01-03 15:57:42 +0000

    Made new accordion to prevent href jumping the scroll

--- app.js
+++ app.js
@@ -10,14 +10,18 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
-const faqBtn = document.querySelector('.faq-bar');
-let faqOpen = false;
-faqBtn.addEventListener('click', () => {
-  if(!faqOpen) {
-    faqBtn.classList.add('open');
-    faqOpen = true;
-  } else {
-    faqBtn.classList.remove('open');
-    faqOpen = false;
-  }
-});
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
